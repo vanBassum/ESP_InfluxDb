2e6da2d32c121b0808feca1c0fda9ccde72bd2e5
Bas Visscher <bas@koolecontrols.nl>
2025-05-22 16:18:12 +0200

    This should work!

--- README.md
+++ README.md
@@ -4,27 +4,36 @@
 This repository contains a custom ESPHome component that posts sensor data to an InfluxDB 2.x instance using HTTP.
 
 
----
-
 ## Features
 
 - Subscribes to all non-internal sensors in ESPHome.
 - Sends sensor data in [InfluxDB line protocol](https://docs.influxdata.com/influxdb/latest/reference/syntax/line-protocol/) format.
 - Includes timestamps in seconds.
 
-
-
 ## Configuration Example
 
 ```yaml
 esphome:
-  name: myInflux
-  platform: esp32
-  board: esp32-c3-devkitm-1
+  name: testjec3
+  friendly_name: TestjeC3
+
+esp32:
+  board: esp32dev
+  framework:
+    type: esp-idf
 
 wifi:
   ssid: !secret wifi_ssid
   password: !secret wifi_password
+
+external_components:
+  - source:
+      type: git
+      url: https://github.com/vanBassum/ESP_InfluxDb
+      
+sensor: 
+  - platform: uptime
+    name: "Uptime"
 
 http_request:
   id: http_component
@@ -38,4 +47,16 @@ influxdb:
   bucket: !secret influxdb_bucket
   measurement: esp_data
   http_request_id: http_component
+
+```
+
+## Secrets Example
+
+```yaml
+wifi_ssid: "MyWifiSSID"
+wifi_password: "WyWifiPassword"
+influxdb_url: http://MyInfluxServer.com:8086/api/v2/write
+influxdb_token: MyInfluxApiKey=
+influxdb_org: MyOrganisation
+influxdb_bucket: MyBucket
 ```
--- components/influxdb/influxdb_writer.cpp
+++ components/influxdb/influxdb_writer.cpp
@@ -30,31 +30,26 @@ namespace esphome
             if (!std::isfinite(state) || this->http_ == nullptr)
                 return;
 
-            // Get current timestamp in seconds
+
             time_t timestamp = time(nullptr);
 
-            // Escape sensor ID for InfluxDB tags
             std::string sensor_id = obj->get_object_id();
             std::replace(sensor_id.begin(), sensor_id.end(), ' ', '_');
 
-            std::string line = this->measurement_ 
-                                + ",sensor=" + sensor_id 
-                                + " value=" + esphome::to_string(state) 
-                                + " " + esphome::to_string(timestamp);
+            std::string line = this->measurement_ +
+                               ",sensor=" + sensor_id +
+                               " value=" + esphome::to_string(state) +
+                               " " + esphome::to_string(timestamp);
 
             std::string full_url = this->url_ + "?org=" + this->org_ + "&bucket=" + this->bucket_ + "&precision=s";
 
             std::list<esphome::http_request::Header> headers = {
-                {"Authorization: Token ", this->token_},
-                {"Content-Type", "text/plain"}
-            };
+                {"Authorization", "Token " + this->token_},
+                {"Content-Type", "text/plain"}};
 
             ESP_LOGD(TAG, "Sending to InfluxDB: %s", line.c_str());
 
-            this->http_->post(
-                full_url,
-                line,
-                headers);
+            this->http_->post(full_url, line, headers);
         }
 
     } // namespace influxdb
